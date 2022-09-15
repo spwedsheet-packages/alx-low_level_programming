b30607d851d2f67a3b784b7017ba33c97a8d25fe
spwedsheet-packages <jasmineinioluwa04@gmail.com>
2022-09-15 14:55:16 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int n);
+void print_triangle(int n);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
