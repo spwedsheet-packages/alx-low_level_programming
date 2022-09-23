5c6465e170d0a58adbb30018cce45b12fb1f9a7f
spwedsheet-packages <jasmineinioluwa04@gmail.com>
2022-09-23 03:12:02 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,6 +12,7 @@
 void print_line(char *c, int s, int l)
 {
 	int j, k;
+
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
@@ -23,10 +24,11 @@ void print_line(char *c, int s, int l)
 	}
 	for (k = 0; k <= s; k++)
 	{
+
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
-		       	putchar('.');
+			putchar('.');
 	}
 }
 
@@ -40,6 +42,7 @@ void print_line(char *c, int s, int l)
 void print_buffer(char *b, int size)
 {
 	int i;
+
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
