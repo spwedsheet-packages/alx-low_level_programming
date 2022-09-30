832131fc9c4066bad16c3396b180de2bb627ce90
spwedsheet-packages <jasmineinioluwa04@gmail.com>
2022-09-30 04:27:49 -0700

    100-change.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main prints out the name of the program
+ * main - prints name of the program
  *
  * @argc: argument count
  * @argv: argument vector
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main prints number of arguments passed to it
+ * main - prints the number of arguments passed into it
  *
  * @argc: argument count
  * @argv: argument vector
